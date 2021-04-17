62d8952f316367e089deac14a0d3c696062c6a97
Andy Kee <andykee@gmail.com>
2021-04-17 10:50:42 -0700

    Use numpy _parse_einsum_input directly

--- loupe/einsum.py
+++ loupe/einsum.py
@@ -1,6 +1,11 @@
 import numpy as np
 
+# This function was implemented in Numpy v1.12 and hasn't
+# substantively changed since then (Numpy v1.20)
+from numpy.core.einsumfunc import _parse_einsum_input
+
 import loupe
+
 
 # The implementation here is heavily influenced by chainer's einsum(). It
 # has been simplified a bit and adapted to work with loupe's interfaces.
@@ -81,138 +86,3 @@ class einsum(loupe.core.Function):
                 
             self.inputs[i].backward(y)
 
-
-einsum_symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-einsum_symbols_set = set(einsum_symbols)
-
-
-# This is a very slightly modified version of an identically named function in Numpy v1.19.0
-def _parse_einsum_input(operands):
-    """
-    A reproduction of einsum c side einsum parsing in python.
-    Returns
-    -------
-    input_strings : str
-        Parsed input strings
-    output_string : str
-        Parsed output string
-    operands : list of array_like
-        The operands to use in the numpy contraction
-    Examples
-    --------
-    The operand list is simplified to reduce printing:
-    >>> np.random.seed(123)
-    >>> a = np.random.rand(4, 4)
-    >>> b = np.random.rand(4, 4, 4)
-    >>> _parse_einsum_input(('...a,...a->...', a, b))
-    ('za,xza', 'xz', [a, b]) # may vary
-    >>> _parse_einsum_input((a, [Ellipsis, 0], b, [Ellipsis, 0]))
-    ('za,xza', 'xz', [a, b]) # may vary
-    """
-
-    if len(operands) == 0:
-        raise ValueError("No input operands")
-
-    if isinstance(operands[0], str):
-        subscripts = operands[0].replace(" ", "")
-        operands = [v for v in operands[1:]] # modified from Numpy's version, which wraps the first v in asanyarray(v)
-
-        # Ensure all characters are valid
-        for s in subscripts:
-            if s in '.,->':
-                continue
-            if s not in einsum_symbols:
-                raise ValueError("Character %s is not a valid symbol." % s)
-                    
-    # Check for proper "->"
-    if ("-" in subscripts) or (">" in subscripts):
-        invalid = (subscripts.count("-") > 1) or (subscripts.count(">") > 1)
-        if invalid or (subscripts.count("->") != 1):
-            raise ValueError("Subscripts can only contain one '->'.")
-
-    # Parse ellipses
-    if "." in subscripts:
-        used = subscripts.replace(".", "").replace(",", "").replace("->", "")
-        unused = list(einsum_symbols_set - set(used))
-        ellipse_inds = "".join(unused)
-        longest = 0
-
-        if "->" in subscripts:
-            input_tmp, output_sub = subscripts.split("->")
-            split_subscripts = input_tmp.split(",")
-            out_sub = True
-        else:
-            split_subscripts = subscripts.split(',')
-            out_sub = False
-
-        for num, sub in enumerate(split_subscripts):
-            if "." in sub:
-                if (sub.count(".") != 3) or (sub.count("...") != 1):
-                    raise ValueError("Invalid Ellipses.")
-
-                # Take into account numerical values
-                if operands[num].shape == ():
-                    ellipse_count = 0
-                else:
-                    ellipse_count = max(operands[num].ndim, 1)
-                    ellipse_count -= (len(sub) - 3)
-
-                if ellipse_count > longest:
-                    longest = ellipse_count
-
-                if ellipse_count < 0:
-                    raise ValueError("Ellipses lengths do not match.")
-                elif ellipse_count == 0:
-                    split_subscripts[num] = sub.replace('...', '')
-                else:
-                    rep_inds = ellipse_inds[-ellipse_count:]
-                    split_subscripts[num] = sub.replace('...', rep_inds)
-
-        subscripts = ",".join(split_subscripts)
-        if longest == 0:
-            out_ellipse = ""
-        else:
-            out_ellipse = ellipse_inds[-longest:]
-
-        if out_sub:
-            subscripts += "->" + output_sub.replace("...", out_ellipse)
-        else:
-            # Special care for outputless ellipses
-            output_subscript = ""
-            tmp_subscripts = subscripts.replace(",", "")
-            for s in sorted(set(tmp_subscripts)):
-                if s not in (einsum_symbols):
-                    raise ValueError("Character %s is not a valid symbol." % s)
-                if tmp_subscripts.count(s) == 1:
-                    output_subscript += s
-            normal_inds = ''.join(sorted(set(output_subscript) -
-                                         set(out_ellipse)))
-
-            subscripts += "->" + out_ellipse + normal_inds
-
-    # Build output string if does not exist
-    if "->" in subscripts:
-        input_subscripts, output_subscript = subscripts.split("->")
-    else:
-        input_subscripts = subscripts
-        # Build output subscripts
-        tmp_subscripts = subscripts.replace(",", "")
-        output_subscript = ""
-        for s in sorted(set(tmp_subscripts)):
-            if s not in einsum_symbols:
-                raise ValueError("Character %s is not a valid symbol." % s)
-            if tmp_subscripts.count(s) == 1:
-                output_subscript += s
-
-    # Make sure output subscripts are in the input
-    for char in output_subscript:
-        if char not in input_subscripts:
-            raise ValueError("Output character %s did not appear in the input"
-                             % char)
-
-    # Make sure number operands is equivalent to the number of terms
-    if len(input_subscripts.split(',')) != len(operands):
-        raise ValueError("Number of einsum subscripts must be equal to the "
-                         "number of operands.")
-
-    return input_subscripts, output_subscript, operands
