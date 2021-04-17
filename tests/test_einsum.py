a31d8365fe78986b6841ecb7d7c1428d2c1dc830
Andy Kee <andykee@gmail.com>
2021-04-17 12:38:10 -0700

    Include einsum.backward tests

--- tests/test_einsum.py
+++ tests/test_einsum.py
@@ -31,3 +31,33 @@ import loupe
 def test_einsum_forward(subs, shapes):
     inputs = [np.random.uniform(low=-1, high=1, size=shape) for shape in shapes]
     assert np.all(loupe.einsum(subs, *inputs).data == np.einsum(subs, *inputs))
+
+
+@pytest.mark.parametrize("subs, shapes, func", [
+    # element-wise multiply
+    ('i,i->i', ((3,),(3,)), loupe.multiply), 
+    ('ij,ij->ij', ((3,3),(3,3)), loupe.multiply)
+])
+def test_einsum_backward_multiply(subs, shapes, func):
+    inputs = [loupe.rand(low=-1, high=1, size=shape, requires_grad=True) for shape in shapes]
+    out = loupe.einsum(subs, *inputs)
+    out.backward(np.ones_like(out))
+
+    einsum_grads = []
+    for x in inputs:
+        einsum_grads.append(x.grad.copy())
+        x.zero_grad()
+
+    # repeat the calculation with an explicit loupe function
+    del out
+    out = func(*inputs)
+    out.backward(np.ones_like(out))
+    loupe_grads = []
+    for x in inputs:
+        loupe_grads.append(x.grad.copy()) 
+
+    assert np.allclose(einsum_grads, loupe_grads)
+
+
+#def test_einsum_backward_basis_expansion():
+#    pass
