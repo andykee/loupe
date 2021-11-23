1b6e3f396358ae23f7988b2d9e4f168c512b95fc
Andy Kee <andykee@gmail.com>
2021-11-22 17:09:20 -0800

    Implement ndim dft2

--- loupe/fourier.py
+++ loupe/fourier.py
@@ -1,6 +1,7 @@
 import functools
 
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 
 import loupe
 
@@ -32,8 +33,8 @@ class ifft2(loupe.core.Function):
 class dft2(loupe.core.Function):
     """Compute the 2-dimensional discrete Fourier Transform.
 
-    This function allows independent control over input shape, output shape, 
-    and output sampling by implementing the matrix triple product algorithm 
+    This function allows independent control over input shape, output shape,
+    and output sampling by implementing the matrix triple product algorithm
     described in [1].
 
     Parameters
@@ -41,15 +42,15 @@ class dft2(loupe.core.Function):
     x : array_like
         Input array.
     alpha : (2,) array_like
-        Output plane sampling frequency in terms of (row, col). 
+        Output plane sampling frequency in terms of (row, col).
     shape : (2,) arrqy_like, optional
-        Shape of the output. If `shape` is None (default), the shape of the input 
+        Shape of the output. If `shape` is None (default), the shape of the input
         is used.
     unitary : bool, optional
-        Normalization flag. If True, a normalization is performed on the output 
+        Normalization flag. If True, a normalization is performed on the output
         such that the DFT operation is unitary. Default is True.
     shift : (2,) array_like, optional
-        Number of pixels in (r,c) to shift the DC pixel in the output plane with 
+        Number of pixels in (r,c) to shift the DC pixel in the output plane with
         the origin centrally located in the plane. Default is (0, 0).
 
     Returns
@@ -61,27 +62,27 @@ class dft2(loupe.core.Function):
     -----
     * `dft2` expects the DC pixel to be centered in the input array.
       The center is assumed to be at ``np.floor(n/2) + 1`` for length `n`.
-      Note this is consistent with a shifted FFT performed as: 
+      Note this is consistent with a shifted FFT performed as:
       ``F = ifftshift(fft2(fftshift(f)))``
-        
-    * Performing the DFT with ``alpha = 1/f.shape``, ``unitary = False``, 
+
+    * Performing the DFT with ``alpha = 1/f.shape``, ``unitary = False``,
       and ``shape = f.shape`` is equivalent to a shifted FFT:
 
     .. code:: pycon
-            
+
         >>> f = loupe.rand(size=(10, 10))
         >>> F = loupe.dft2(f, alpha=(1/10, 1/10), unitary=False, shape=f.shape)
-        >>> G = np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(f))) 
+        >>> G = np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(f)))
         >>> np.allclose(F, G)
         True
 
     References
     ----------
     [1] Soummer, et. al. Fast computation of Lyot-style coronagraph propagation (2007)
-        
+
     """
     def __init__(self, x, alpha, shape=None, unitary=True, shift=(0,0)):
-        
+
         self.input = loupe.asarray(x)
         self.alpha = loupe.asarray(alpha)
         self.output_shape = shape
@@ -94,18 +95,22 @@ class dft2(loupe.core.Function):
         alpha = _sanitize_ordered_pair(self.alpha.getdata(), dtype=float)
         shift = _sanitize_ordered_pair(self.shift, dtype=float)
 
-        if self.output_shape is None:
-            shape = input.shape
-        else:
-            shape = self.output_shape
+        shape, strides = _as_strided_args(input, self.output_shape)
+        result = np.empty(shape, dtype=complex)
+        W_row, W_row_conj, W_col, W_col_conj = [], [], [], []
 
-        W_row = _dft2_matrix(input.shape[0], shape[0], alpha[0], shift[0])
-        W_row_conj = np.conj(W_row)
+        for n, x in enumerate(as_strided(input, shape=shape, strides=strides, writeable=False)):
+            Wr = _dft2_matrix(x.shape[0], shape[1], alpha[0], shift[0])
+            Wc = _dft2_matrix(x.shape[1], shape[2], alpha[1], shift[1])
+            tmp = np.dot(Wr.T, x)
+            result[n] = np.dot(tmp, Wc)
 
-        W_col = _dft2_matrix(input.shape[1], shape[1], alpha[1], shift[1])
-        W_col_conj = np.conj(W_col)
+            W_row.append(Wr)
+            W_row_conj.append(np.conj(Wr))
+            W_col.append(Wc)
+            W_col_conj.append(np.conj(Wc))
 
-        result = np.dot(W_row.T, input.dot(W_col))
+        result = np.squeeze(result)
 
         if self.unitary:
             root_alpha = np.sqrt(np.abs(alpha))
@@ -122,7 +127,7 @@ class dft2(loupe.core.Function):
 
         if self.unitary:
             result *= norm_coeff
-        
+
         return result
 
     def backward(self, grad):
@@ -137,9 +142,14 @@ class dft2(loupe.core.Function):
         # always multiply grad by norm_coeff here.
         grad_clean = grad * norm_coeff
 
-        # h_grad is the dot product of W_row_conj and the "true" gradient. 
-        h_grad = np.dot(W_row_conj, grad_clean)
-        input_grad = np.dot(h_grad, W_col_conj.T)
+        shape, strides = _as_strided_args(grad_clean, input.shape[-2:])
+        input_grad = np.empty(shape, dtype=complex)
+
+        for n, x in enumerate(as_strided(grad_clean, shape=shape, strides=strides, writeable=False)):
+            h_grad = np.dot(W_row_conj[n], x)
+            input_grad[n] = np.dot(h_grad, W_col_conj[n].T)
+
+        input_grad = np.squeeze(input_grad)
 
         self.input.backward(input_grad)
 
@@ -154,14 +164,28 @@ def _dft2_matrix(n, N, alpha, shift):
 @functools.lru_cache(maxsize=32)
 def _coords(n):
     #TODO: ensure n is a python primitive type
-    # It needs to be hashable for LRU cache to work 
+    # It needs to be hashable for LRU cache to work
     return np.arange(n) - np.floor(n/2.0)
 
 
 def _sanitize_ordered_pair(x, dtype):
     x = np.asarray(x)
-    
+
     if x.shape != (2,):
             raise ValueError(f"can't interpret x with shape {x.shape} as ordered pair")
-    
+
     return [dtype(x[0]), dtype(x[1])]
+
+
+def _as_strided_args(input, output_shape):
+    depth = 1 if input.ndim ==2 else input.shape[0]
+
+    shape = output_shape if output_shape is not None else input.shape[-2:]
+    if len(input.shape) == 2:
+        shape = (1, *shape)
+    else:
+        shape = (input.shape[0], *shape)
+
+    strides = input.strides if depth > 1 else (0, *input.strides)
+    print(shape, strides)
+    return shape, strides
