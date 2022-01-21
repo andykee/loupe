1150d24fbafd2266071603087f4fb7988d210b11
Andy Kee <andykee@gmail.com>
2022-01-21 13:42:13 -0800

    Increment version number

--- loupe/__init__.py
+++ loupe/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from loupe.core import array
 
@@ -9,11 +9,11 @@ from loupe.einsum import einsum
 from loupe.fourier import dft2
 
 from loupe.numeric import (
-    add, 
-    subtract, 
-    multiply, 
-    power, 
-    exp, 
+    add,
+    subtract,
+    multiply,
+    power,
+    exp,
     expc,
     sum,
     slice
@@ -28,23 +28,23 @@ from loupe.special import absolute_square, abs_square
 from loupe.tensordot import tensordot
 
 from loupe.util import (
-    asarray, 
-    zeros, 
-    zeros_like, 
-    ones, 
-    ones_like, 
-    rand, 
-    randn, 
+    asarray,
+    zeros,
+    zeros_like,
+    ones,
+    ones_like,
+    rand,
+    randn,
     centroid,
     shift,
     register
 )
 
 from loupe.zernike import (
-    zernike, 
-    zernike_compose, 
-    zernike_basis, 
-    zernike_fit, 
+    zernike,
+    zernike_compose,
+    zernike_basis,
+    zernike_fit,
     zernike_remove,
     zernike_coordinates
 )
